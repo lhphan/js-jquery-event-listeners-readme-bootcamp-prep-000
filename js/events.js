c8358bc1c4b6f2912ea4ab297bee78013202da22
Lien Phan <whyofcourse@gmail.com>
2017-09-15 01:57:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,19 @@
-//define functions here
-
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+function frameIt(){
+  $('img').on('load', function(){
+    
+  })
+}
+function pressIt(){
+  
+}
+function submitIt(){
+  
+}
 $(document).ready(function(){
 
 // call functions here
